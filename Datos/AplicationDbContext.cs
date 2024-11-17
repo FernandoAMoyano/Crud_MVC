7d493cc865b7b096811b66c6ec2bf5c622b3726b
FernandoAMoyano <fernandomoyano21@gmail.com>
2024-11-17 16:46:58 -0300

    first commit

---
+++ Datos/AplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CrudMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudMVC.Datos
+{
+    public class AplicationDbContext(DbContextOptions<AplicationDbContext> options) : DbContext(options)
+    {
+
+        //Agregar Los modelos aquí
+        public DbSet<Contacto> Contacto { get; set; }
+    }
+}
---
+++ Models/Contacto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudMVC.Models
+{
+    public class Contacto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage="El nombre es obligatorio")]
+        public required string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El Telefono es obligatorio")]
+        public required string Telefono { get; set; }
+
+        [Required(ErrorMessage = "El Celular es obligatorio")]
+        public required string Celular { get; set; }
+
+        [Required(ErrorMessage = "El Email es obligatorio")]
+        public required string Email { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+    }
+}
